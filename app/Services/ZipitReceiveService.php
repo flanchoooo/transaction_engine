948c3519eeabc57ed88ad1ac39630769b6ee48c8
flavian <flavianmachimbirike@gmail.com>
2020-03-09 14:18:02 +0200

    Narration fix

--- app/Services/ZipitReceiveService.php
+++ app/Services/ZipitReceiveService.php
@@ -19,7 +19,7 @@ class ZipitReceiveService
             'our_branch_id'         => $branch_id,
             'account_id'            => $account_number,
             'trx_description_id'    => '007',
-            'trx_description'       => "SP | Zipit receive | $narration | $rrn ",
+            'trx_description'       => "SP | Zipit receive | $narration | $rrn |$reference ",
             'trx_amount'            => $amount);
 
 
@@ -28,7 +28,7 @@ class ZipitReceiveService
             'our_branch_id'         =>$branch_id,
             'account_id'            => ZIMSWITCH,
             'trx_description_id'    => '008',
-            'trx_description'       => "SP | Zipit receive: $rrn account:$account_number",
+            'trx_description'       => "SP | Zipit receive | $rrn |$account_number | $reference",
             'trx_amount'            =>   -$amount);
 
 
