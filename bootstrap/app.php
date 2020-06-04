29f67bb7721f71ceadfef4fb8a5b693f2928db8a
TawanaNyasha <flavianmachimbirke@gmail.com>
2020-06-05 00:13:13 +0200

    Disabled CORS.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 require_once __DIR__ . '/transaction_type.php';
 
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
-
-
 try {
     (new Dotenv\Dotenv(dirname(__DIR__)))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
@@ -69,7 +62,8 @@ $app->singleton(
 */
 
  $app->middleware([
-     App\Http\Middleware\ExampleMiddleware::class
+     App\Http\Middleware\ExampleMiddleware::class,
+     App\Http\Middleware\CorsMiddleware::class
  ]);
 
  $app->routeMiddleware([
