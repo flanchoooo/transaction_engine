9768cce1e35f421cd29dedc7c9697aad146e31cf
TawanaNyasha <flavianmachimbirke@gmail.com>
2020-06-04 11:06:53 +0200

    Disabled CORS.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,9 @@ try {
 | application as an "IoC" container and router for this framework.
 |
 */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
